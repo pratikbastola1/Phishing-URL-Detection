a04a74c0592e76512baea37709aaee49d40c19cd
Pratik Bastola <pratikbastola@Pratik.local>
2023-08-29 14:58:21 +1000

    test

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
-# importing required libraries
-
 from flask import Flask, request, render_template
 import numpy as np
-import pandas as pd
-from sklearn import metrics
 import warnings
 import pickle
-
-warnings.filterwarnings('ignore')
 from feature import FeatureExtraction
 
-file = open("pickle/model.pkl", "rb")
-gbc = pickle.load(file)
-file.close()
+# Turn off Python warnings
+warnings.filterwarnings('ignore')
+
+# Load the model only once when the application starts
+with open("pickle/model.pkl", "rb") as file:
+    gbc = pickle.load(file)
 
 app = Flask(__name__)
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -23,18 +21,14 @@ def index():
         obj = FeatureExtraction(url)
         x = np.array(obj.getFeaturesList()).reshape(1, 30)
 
-        y_pred = gbc.predict(x)[0]
-        # 1 is safe
-        # -1 is unsafe
-        y_pro_phishing = gbc.predict_proba(x)[0, 0]
         y_pro_non_phishing = gbc.predict_proba(x)[0, 1]
-        # if(y_pred ==1 ):
-        pred = "It is {0:.2f} % safe to go ".format(y_pro_phishing * 100)
+
         return render_template('index.html', xx=round(y_pro_non_phishing, 2), url=url)
+
     return render_template("index.html", xx=-1)
 
 
 if __name__ == "__main__":
+    # Remember to turn off debug mode when deploying to production
     app.run(debug=True)
-
-
+ss
